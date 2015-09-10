d2b137dd8f6db614abac373c644c0d6fe3a51075
seon <piaoshihuang@wanleyun.com>
2015-09-10 17:02:57 +0800

    init

---
+++ bin/cli.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var cp = require('child_process');
+var fs = require('fs');
+var _ = require('underscore');
+var mkdirp = require('mkdirp');
+var path = require('path');
+
+var argv = process.argv;
+
+var installAll = function() {
+  var cpArgv = process.argv.slice(2);
+  cp.spawnSync('npm', cpArgv, {
+    env: process.env,
+    stdio: 'inherit'
+  });
+};
+
+var installPackage = function(package, version) {
+  package = package + '@' + version;
+  cp.spawnSync('npm', ['install', package], {
+    env: process.env,
+    stdio: 'inherit'
+  });
+};
+
+if (argv[2] === 'install') {
+  //npmto install
+  if (argv.length === 3) {
+    installAll();
+    var settings = JSON.parse(fs.readFileSync('package.json'));
+    var npmtoPackage = settings.npmto || {
+      "padnum": {
+        "version": "~0.0.1",
+        "to": "pages"
+      }
+    };
+    _.each(npmtoPackage, function(config, name) {
+      installPackage(name, config.version);
+      var to = path.join(config.to, name);
+      mkdirp.sync(to);
+      fs.renameSync(path.join('node_modules', name), to);
+    });
+  } else {
+    //当存在--save类参数时，commander会把后面的第一个package name当做参数值一块去掉
+    argv = argv.filter(function(arg) {
+      return arg.indexOf('--save') !== 0;
+    });
+    program
+      .version('0.0.1')
+      .command('install [otherPackages...]')
+      .allowUnknownOption(true)
+      .action(function(packages) {
+        if (packages) {
+          packages.forEach(function(oPackage) {
+            console.log('package %s', oPackage);
+          });
+        }
+      });
+    program.parse(argv);
+  }
+}
---
+++ pages/padnum/index.js
@@ -0,0 +1,7 @@
+function pad(number, width, padding) {
+  padding = padding || '0';
+  number = number + '';
+  var length = number.length;
+  return !width || length >= width ? '' + number : new Array(width - length + 1).join(padding) + number;
+}
+module.exports = pad;
